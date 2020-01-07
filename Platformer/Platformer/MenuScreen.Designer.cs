108104074ca270ce7e5ccc318dff67a646f462e2
adawson21 <aad4wson@gmail.com>
2020-01-07 14:51:56 -0600

    working on main menu

---
+++ Platformer/Platformer/MenuScreen.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Platformer
+{
+    partial class MenuScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_loadGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_loadGame
+            // 
+            this.btn_loadGame.Location = new System.Drawing.Point(363, 203);
+            this.btn_loadGame.Name = "btn_loadGame";
+            this.btn_loadGame.Size = new System.Drawing.Size(75, 23);
+            this.btn_loadGame.TabIndex = 0;
+            this.btn_loadGame.Text = "Start Game";
+            this.btn_loadGame.UseVisualStyleBackColor = true;
+            this.btn_loadGame.Click += new System.EventHandler(this.btn_loadGame_Click);
+            // 
+            // MenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_loadGame);
+            this.Name = "MenuScreen";
+            this.Text = "MenuScreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_loadGame;
+    }
+}
--- Platformer/Platformer/Properties/Resources.Designer.cs
+++ Platformer/Platformer/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Platformer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Background {
+            get {
+                object obj = ResourceManager.GetObject("Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Grass {
             get {
                 object obj = ResourceManager.GetObject("Grass", resourceCulture);
